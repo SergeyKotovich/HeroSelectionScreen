6af68eda96fe400d2cfd0428b924ec19e31fde50
SergeyKotovich <115530111+SergeyKotovich@users.noreply.github.com>
2023-12-17 11:23:13 +0300

    feat

--- Assets/Scripts/HeroSettings.cs
+++ Assets/Scripts/HeroSettings.cs
@@ -1,7 +1,11 @@
 using System;
+using UnityEngine;
 
 [Serializable]
-public class HeroSettings 
+public class HeroSettings
 {
-    
+    [SerializeField] private float _health;
+    [SerializeField] private float _attack;
+    [SerializeField] private float _defense;
+    [SerializeField] private float _speed;
 }
